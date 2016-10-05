15647cffe4c7c59f8801c41f8a9c16686f3ae193
gerhard4671 <ostral64@yahoo.com>
2016-10-06 02:05:04 +0300

    Исправил пробелы

--- src/js/check.js
+++ src/js/check.js
@@ -1,5 +1,5 @@
 
-function getMessage (a,b) {
+function getMessage (a, b) {
 
     if (typeof(a) === "boolean") {
         if (a === true) {
@@ -7,29 +7,22 @@ function getMessage (a,b) {
         } else {
             return "Переданное GIF-изображение не анимировано";
         }
+
     } else if (typeof(a) === "number") {
-        return "Переданное SVG-изображение содержит " + a + " объектов и "+ (b * 4) +" атрибутов";
+        return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
     } else if (Array.isArray(a) && !Array.isArray(b)) { //Добавил условие на 2 аргумент, так как пересекается со след условием
         var amountOfRedPoints = 0;
         for (var i = 0; i < a.length; i++ ) {
             amountOfRedPoints += a[i];
         }
-        return "Количество красных точек во всех строчках изображения: "+ amountOfRedPoints;
+        return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
     } else if ( Array.isArray(a) &&  Array.isArray(b) ) {
         var artifactsSquare = 0;
         for  (var j = 0; j < a.length; j++) {
-            artifactsSquare +=(a[j] * b[j]);
+            artifactsSquare += (a[j] * b[j]);
         }
-        return "Общая площадь артефактов сжатия: "+ artifactsSquare +" пикселей";
+        return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
     } else {
         return "Переданы некорректные данные";
     }
 }
-
-// Тесты для консоли
-// console.log(getMessage(true, false));
-// console.log(getMessage(false, false));
-// console.log(getMessage(15, 6));
-// console.log(getMessage([3, 5, 6], false));
-// console.log(getMessage([1, 2, 3], [4, 5, 6]));
-// console.log(getMessage("sda", "asd"));
