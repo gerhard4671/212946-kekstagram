1c0071ee6334e6f021d8087ef83a9161673a6b6f
gerhard4671 <ostral64@yahoo.com>
2016-11-18 20:47:49 +0200

    localStorage complete

--- src/js/upload.js
+++ src/js/upload.js
@@ -1,4 +1,3 @@
-/* global Resizer: true */
 
 /**
  * @fileoverview
@@ -84,7 +83,19 @@ define(function() {
       backgroundElement.style.backgroundImage = 'url(' + images[randomImageNumber] + ')';
     };
 
+    var expireDate = function() {
+      var today = new Date();
+      today.setHours(0, 0, 0, 0);
 
+      var dateOfGHbirth = new Date();
+      dateOfGHbirth.setDate(9);
+      dateOfGHbirth.setMonth(11);
+      dateOfGHbirth.setHours(0, 0, 0, 0);
+      if (today - dateOfGHbirth <= 0) {
+        dateOfGHbirth.setFullYear(dateOfGHbirth.getFullYear() - 1);
+      }
+      return Math.floor( (today - dateOfGHbirth) / (1000 * 60 * 60 * 24));
+    };
 
     /**
      * Проверяет, валидны ли данные, в форме кадрирования.
@@ -268,8 +279,8 @@ define(function() {
           }
 
           filterImage.src = image;
-          filterForm['upload-filter'].value = localStorage.getItem('upload-filter');
-          filterImage.className = 'filter-image-preview ' + 'filter-' + localStorage.getItem('upload-filter');
+          filterForm['upload-filter'].value = window.Cookies.get('upload-filter');
+          filterImage.className = 'filter-image-preview ' + 'filter-' + window.Cookies.get('upload-filter');
 
           resizeForm.classList.add('invisible');
           filterForm.classList.remove('invisible');
@@ -308,7 +319,7 @@ define(function() {
         return item.checked;
       })[0].value;
 
-      localStorage.setItem('upload-filter', String(selectedFilter));
+      window.Cookies.set('upload-filter', '' + selectedFilter + '', { expires: expireDate()});
 
       // Класс перезаписывается, а не обновляется через classList потому что нужно
       // убрать предыдущий примененный класс. Для этого нужно или запоминать его
