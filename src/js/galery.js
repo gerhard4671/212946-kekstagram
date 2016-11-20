5c3f8b4856ffb8b5077db51ecdbd21de3108fccd
gerhard4671 <ostral64@yahoo.com>
2016-11-20 13:48:25 +0200

    rewrite objects with prototypes

--- src/js/galery.js
+++ src/js/galery.js
@@ -3,6 +3,7 @@
 
 define(function() {
 //Объект Галерея
+
   var Galery = function(data) {
     this.data = data;
     this.activePicture = null;
@@ -11,6 +12,10 @@ define(function() {
     this.overlay = document.querySelector('.gallery-overlay');
     this.overlayClose = document.querySelector('.gallery-overlay-close');
     this.overlayImage = document.querySelector('.gallery-overlay-image');
+
+    this.clickHandler = this.clickHandler.bind(this);
+
+
   };
 
 //В файле pictures.js передаем в метод галереи значение - данные из load.js
@@ -21,26 +26,31 @@ define(function() {
 // В файле picture.js передаем в метод галереи show значение - Номер фотки по которой произошел клик, а он в свою очередь
 // является параметром функции колбека при обходе массива с данными)
   Galery.prototype.show = function(number) {
-    var self = this;
     this.overlay.classList.remove('invisible');
-// -----------------обработчики событий---------------------------------------------------------
-    this.overlay.onclick = function(evt) {
-// Если нажали на кнопку закрыть
-      if (evt.target === self.overlayClose) {
-        self.hide();
-      }
-// Если нажали на фотографию, передаем в метод галерие setActivePicture номер фотографии = текущий+1(следубщая)
-      if (evt.target === self.overlayImage) {
-        self.setActivePicture(self.activePicture + 1);
-      }
-    };
+    this.overlay.addEventListener('click', this.clickHandler);
     this.setActivePicture(number);
+  };
+
+//-----------------обработчик событий---------------------------------------------------------
+  Galery.prototype.clickHandler = function(evt) {
+// Если крестик, закрываем галерею
+    if(evt.target === this.overlayClose) {
+      this.hide();
+    }
+// Если нажали на фотографию, передаем в метод галерие setActivePicture номер фотографии = текущий+1(следубщая)
+    if (evt.target === this.overlayImage) {
+      this.setActivePicture(this.activePicture + 1);
+    }
   };
 
 // Метод галереи hide - скрывает Галерею и обнуляет обработчик  событий
   Galery.prototype.hide = function() {
     this.overlay.classList.add('invisible');
-    this.overlay.onclick = null;
+    this.removeHandlers();
+  };
+
+  Galery.prototype.removeHandlers = function() {
+    this.overlayClose.removeEventListener('click', this.clickHandler);
   };
 
 // Метод галереи setActivePicture - Проверяем если передан последний элемент в массиве pictures - то ставим активный элемент = 0
